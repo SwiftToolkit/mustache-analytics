8b864f00c76f1a3feeaeb4dca723ec4b68a3e5a3
Natan Rolnik <natanr@monday.com>
2025-01-24 01:38:08 +0200

    Add option to generate Kotlin files

--- Sources/AnalyticsGenerator.swift
+++ Sources/AnalyticsGenerator.swift
@@ -8,11 +8,14 @@ struct AnalyticsGenerator: ParsableCommand {
     @Option(name: .shortAndLong)
     var input: String
 
+    @Option
+    var format: Format
+
     func run() throws {
         let events = try loadEvents()
         let generatedCode = try generate(using: events)
 
-        let outputPath = FileManager.default.currentDirectoryPath + "/Events.swift"
+        let outputPath = FileManager.default.currentDirectoryPath + "/Events." + format.fileExtension
         try generatedCode.write(
             to: URL(fileURLWithPath: outputPath),
             atomically: true,
@@ -28,7 +31,7 @@ struct AnalyticsGenerator: ParsableCommand {
 
     func generate(using events: [AnalyticEvent]) throws -> String {
         guard let templateURL = Bundle.module
-            .url(forResource: "EventsSwift", withExtension: ".mustache") else {
+            .url(forResource: format.templateName, withExtension: ".mustache") else {
             fatalError("Template file not found")
         }
 
@@ -36,5 +39,28 @@ struct AnalyticsGenerator: ParsableCommand {
         let template = try MustacheTemplate(string: templateString)
 
         return template.render(["events": events])
+    }
+}
+
+extension AnalyticsGenerator {
+    enum Format: String, ExpressibleByArgument {
+        case swift
+        case kotlin
+    }
+}
+
+extension AnalyticsGenerator.Format {
+    var templateName: String {
+        switch self {
+        case .swift: "EventsSwift"
+        case .kotlin: "EventsKotlin"
+        }
+    }
+
+    var fileExtension: String {
+        switch self {
+        case .swift: "swift"
+        case .kotlin: "kt"
+        }
     }
 }
